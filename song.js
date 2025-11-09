bc33721dfb5e2dd7460b8d2e036e80533af362a7
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2025-11-09 10:11:30 -0300

    Primeira Oitava

--- song.js
+++ song.js
@@ -12,53 +12,79 @@ const noteAb = new Audio("SOURCE/piano-mp3/Ab4.mp3");
 const noteA = new Audio("SOURCE/piano-mp3/A4.mp3");
 const noteBb = new Audio("SOURCE/piano-mp3/Bb4.mp3");
 const noteB = new Audio("SOURCE/piano-mp3/B4.mp3");
+const noteC5 = new Audio("SOURCE/piano-mp3/C5.mp3");
 
-// teclas 
+// teclas
 
 let keyC = document.getElementById("do-C");
-keyC.addEventListener("click", function(){
-    noteC.play();
-})
+keyC.addEventListener("click", function () {
+  noteC.play();
+});
 let keyDb = document.getElementById("reBemol-Db");
 
-keyDb.addEventListener("click", function(){
-noteDb.play();
-})
+keyDb.addEventListener("click", function () {
+  noteDb.play();
+});
 let keyD = document.getElementById("re-D");
 
-keyD.addEventListener("click", function(){
-noteD.play();
-})
+keyD.addEventListener("click", function () {
+  noteD.play();
+});
 
 let keyEb = document.getElementById("miBemol-Eb");
 
-keyEb.addEventListener("click", function(){
-    noteEb.play();
-})
+keyEb.addEventListener("click", function () {
+  noteEb.play();
+});
 
 let keyE = document.getElementById("mi-E");
 
-keyE.addEventListener("click", function(){
-noteE.play();
-})
+keyE.addEventListener("click", function () {
+  noteE.play();
+});
 
 let keyF = document.getElementById("fa-F");
-keyF.addEventListener("click", function(){
-    noteF.play();
-})
+keyF.addEventListener("click", function () {
+  noteF.play();
+});
 let keyGb = document.getElementById("solBemol-Gb");
 
-keyGb.addEventListener("click", function(){
-    noteGb.play();
-})
+keyGb.addEventListener("click", function () {
+  noteGb.play();
+});
 
 let keyG = document.getElementById("sol-G");
 
-keyG.addEventListener("click", function(){
-noteG.play();
-})
-   
-    
+keyG.addEventListener("click", function () {
+  noteG.play();
+});
 
+let keyAb = document.getElementById(`laBemol-Ab`);
 
+keyAb.addEventListener("click", function () {
+  noteAb.play();
+});
 
+let keyA = document.getElementById(`la-A`);
+
+keyA.addEventListener("click", function () {
+  noteA.play();
+});
+
+let keyBb = document.getElementById(`siBemol-Bb`);
+
+keyBb.addEventListener("click", function () {
+  noteBb.play();
+});
+
+let keyB = document.getElementById(`si-B`);
+
+keyB.addEventListener("click", function () {
+  noteB.play();
+});
+
+let keyC5 = document.getElementById(`proximo_do-C`);
+
+keyC5.addEventListener("click", function () {
+  noteC5.play();
+});
